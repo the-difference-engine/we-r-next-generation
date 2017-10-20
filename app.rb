a26f60393756c5da874a396c3479398f826ea48c
victorclee <victorchristopherlee@gmail.com>
2017-10-19 20:31:59 -0500

    Application test

--- app.rb
+++ app.rb
@@ -21,6 +21,38 @@ end
 
 # post '/api/v1/users/:user_id/profile'
 
-# get '/api/v1/applications/volunteers'
+get '/api/v1/applications/volunteers' do
 
-# post '/api/v1/applications/volunteers'
+{
+  volunteers: [
+    {
+    full_name: "Victor Lee",
+    email: "vlee@gmail.com",
+    address: "1 Chicago Ave",
+    phone_number: "312-345-6655",
+    bio: "Hey",
+    signature: "VL",
+    camp_id: "1",
+    status: "Active",
+    user_id: 1
+    }
+  ]
+}.to_json
+end
+
+
+post '/api/v1/applications/volunteers' do
+
+{
+  full_name: "Natale Anfuso",
+  email: "nanfuso@gmail.com",
+  address: "3 Clark St",
+  phone_number: "312-995-5832",
+  bio: "Hi",
+  signature: "NA",
+  camp_id: "2",
+  status: "Active",
+  user_id: 2
+}.to_json
+
+end
--- spec/e2e/api/v1/applications_volunteers_spec.rb
+++ spec/e2e/api/v1/applications_volunteers_spec.rb
@@ -1,3 +1,48 @@
 # spec/e2e/api/v1/users_profile_spec.rb
 
 # todo: impediment
+# Pull in the e2e helper for all the test functionality we need
+require_relative '../../../e2e_helper.rb'
+
+# Notice the spec name ending in `E2E` this is important, it lets minitest know we want this test to be a `E2eTest`
+describe 'UsersProfileE2E' do
+
+
+  describe 'GET /api/v1/applications/volunteers' do
+    before { get '/api/v1/applications/volunteers' }
+    let(:json) { json_parse(last_response.body) }
+
+      it 'have a full_name property with the value of "Victor Lee"' do
+        json[:volunteers][0][:full_name].must_equal 'Victor Lee'
+        # ... other fields e.g. address_1, zip, country, email, phone_number etc.
+      end
+  end
+
+  describe 'POST' do
+    before do
+      post_json('/api/v1/applications/volunteers', 
+        {
+          full_name: "Natale Anfuso",
+          email: "nanfuso@gmail.com",
+          address: "3 Clark St",
+          phone_number: "312-995-5832",
+          bio: "Hi",
+          signature: "NA",
+          camp_id: "2",
+          status: "Active",
+          user_id: 2
+        }
+        )
+    end
+
+    it 'responds successfully' do
+      last_response.status.must_equal 200
+    end
+  end
+
+
+
+
+
+
+end
