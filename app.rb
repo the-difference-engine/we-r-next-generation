b5c4aca883c5f81b9bd71139933b8530eaf583d7
victorclee <victorchristopherlee@gmail.com>
2017-11-16 20:09:46 -0600

    volunteer delete endpoint done

--- app.rb
+++ app.rb
@@ -21,36 +21,6 @@ post '/api/v1/hello' do
   json(record)
 end
 
-# Volunteer Applications
-
-# post new
-profile_cnter = 0
-post '/api/v1/profiles' do
-  json database[:profiles].insert_one(params)
-end
-
-get '/api/v1/profiles/:profile_id' do
-  profile_id = params[:profile_id]
-  obj_id = BSON::ObjectId(profile_id)
-  profile_table = database[:profiles]
-  query_reults = profile_table.find(:_id => obj_id)
-  match = query_reults.first
-  json(match.to_h)
-
-end
-
-# get all
-get '/api/v1/profiles' do
-    data=[]
-    database[:profiles].find.each do |people|
-      data << people.to_h
-    end
-  json(data)
-end
-
-#update 1
-
-
 get '/api/v1/applications/volunteers' do
   data = []
   database[:volunteers].find.each do |volunteer|
@@ -60,11 +30,9 @@ get '/api/v1/applications/volunteers' do
 end
 
 
-
 get '/api/v1/applications/volunteers/:_id' do
   json database[:volunteers].find(:_id => BSON::ObjectId(params[:_id])).first
 end
-
 
 
 post '/api/v1/applications/volunteers' do
@@ -72,40 +40,19 @@ post '/api/v1/applications/volunteers' do
 end
 
 
-
 put '/api/v1/applications/volunteers/:id' do
-  database[:volunteers].find(:_id => BSON::ObjectId(params[:_id]))
+  idnumber = params.delete("id")
+  json database[:volunteers].update_one(
+    {'_id' => BSON::ObjectId(idnumber)}, {'$set' => params }
+    )
 end
 
-# Camp Applications
 
-get '/api/v1/applications/camps' do
+delete '/api/v1/applications/volunteers/:_id' do
+  database[:volunteers].delete_one( {_id: BSON::ObjectId(params[:_id]) } )
   data = []
-  database[:camps].find.each do |document|
-    data << document.to_h
+  database[:volunteers].find.each do |volunteer|
+    data << volunteer.to_h
   end
   json data
-end
-
-post '/api/v1/applications/camps' do
-  json database[:camps].insert_one(params)
-end
-
-get '/api/v1/applications/camps/:_id' do
-
-  json database[:camps].find(:_id => BSON::ObjectId(params[:_id])).first
-
-end
-
-put '/api/v1/applications/camps/:_id' do
-id_number = params.delete("_id")
-
-json database[:camps].update_one( { '_id' => BSON::ObjectId(id_number) },   { '$set' => params})
-
-end
-
-delete '/api/v1/applications/camps/:_id' do
-
- database[:camps].delete_one( {_id: BSON::ObjectId(params[:_id]) } )
-
 end
