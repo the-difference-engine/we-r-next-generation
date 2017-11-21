9e00d4f666dc025496bf6c8727f0199c0bcf997b
victorclee <victorchristopherlee@gmail.com>
2017-11-20 20:03:25 -0600

    merged all endpoints

--- app.rb
+++ app.rb
@@ -21,6 +21,79 @@ post '/api/v1/hello' do
   json(record)
 end
 
+# Profile endpoints
+# post new
+
+post '/api/v1/profiles' do
+  json database[:profiles].insert_one(params)
+end
+
+get '/api/v1/profiles/:profile_id' do
+  profile_id = params[:profile_id]
+  obj_id = BSON::ObjectId(profile_id)
+  profile_table = database[:profiles]
+  query_reults = profile_table.find(:_id => obj_id)
+  match = query_reults.first
+  json(match.to_h)
+
+end
+
+# get all
+get '/api/v1/profiles' do
+  data=[]
+  database[:profiles].find.each do |people|
+    data << people.to_h
+  end
+  json(data)
+end
+
+put '/api/v1/profiles/:id' do
+  idnumber = params.delete("id")
+  json database[:profiles].update_one(
+    {'_id' => BSON::ObjectId(idnumber)}, {'$set' => params }
+  )
+end
+
+delete '/api/v1/profiles/:_id' do
+
+  database[:profiles].delete_one( {_id: BSON::ObjectId(params[:_id]) } )
+
+end
+
+# Camp endpoints
+get '/api/v1/applications/camps' do
+  data = []
+  database[:camps].find.each do |document|
+    data << document.to_h
+  end
+  json data
+end
+
+post '/api/v1/applications/camps' do
+  json database[:camps].insert_one(params)
+end
+
+get '/api/v1/applications/camps/:_id' do
+
+  json database[:camps].find(:_id => BSON::ObjectId(params[:_id])).first
+
+end
+
+put '/api/v1/applications/camps/:_id' do
+  id_number = params.delete("_id")
+
+  json database[:camps].update_one( { '_id' => BSON::ObjectId(id_number) },   { '$set' => params})
+
+end
+
+delete '/api/v1/applications/camps/:_id' do
+
+  database[:camps].delete_one( {_id: BSON::ObjectId(params[:_id]) } )
+
+end
+
+# Volunteer endpoints
+
 get '/api/v1/applications/volunteers' do
   data = []
   database[:volunteers].find.each do |volunteer|
