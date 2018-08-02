f365997a2d626bd6bf6d00d55c69c5bf03edfa1b
Michael Kirlin <michael.kirlin@here.com>
2018-08-02 18:57:17 -0500

    Adding application models

--- models/applications.rb
+++ models/applications.rb
@@ -1,5 +1,21 @@
 class Application
   include Mongoid::Document
 
-  field :field_name, type: String
+  field :full_name, type: String
+  field :email, type: String
+  field :type, type: String
+  field :address_line_1, type: String
+  field :address_line_2, type: String
+  field :city, type: String
+  field :state_province, type: String
+  field :zip_code, type: String
+  field :country, type: String
+  field :phone_number, type: String
+  field :bio, type: String
+  field :camp, type: String
+  field :date_signed, type: String
+  field :status, type: String
+  field :childName, type: String
+  field :age, type: String
+  field :gender, type: String
 end
