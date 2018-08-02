4c2b9a5d6c0897dc90202ea167c7f0d527dc006b
Michael Kirlin <michael.kirlin@here.com>
2018-08-02 00:47:08 -0500

    Updating profile model and converting profile routes to use Mongoid

--- models/profiles.rb
+++ models/profiles.rb
@@ -3,10 +3,13 @@ class Profile
 
   field :full_name, type: String
   field :email, type: String
+  field :password_hash, type: String
+  field :reset_token, type: String
   field :role, type: String
   field :address, type: String
   field :phone_number, type: String
   field :signature, type: String
   field :camp_id, type: String
   field :status, type: String
+  field :active, type: Boolean
 end
--- routes/profiles.rb
+++ routes/profiles.rb
@@ -9,140 +9,121 @@ module Sinatra
           signupParams = ['name', 'email', 'password', 'password_hash']
 
           get_all_profiles = lambda do
-            data=[]
-            DATABASE[:profiles].find.each do |profile|
-              data << profile.to_h
-            end
-            json(data)
+            json(Profile.all())
           end
 
           create_a_profile = lambda do
-            newProfile = params
-            newProfile['password_hash'] = createPasswordHash(params['password'])
-            if !checkSignupParameters(newProfile, signupParams)
-              halt 400, "the requirements were not met, did not post to database"
-            elsif DATABASE[:profiles].find(:email => newProfile['email']).first
-              halt 400, "a profile with this email address already exists"
+            profile = params
+            profile['password_hash'] = createPasswordHash(params['password'])
+            if !checkSignupParameters(profile, signupParams)
+              halt 400, "Parameter requirements were not met."
+            elsif Profile.find_by(email: profile['email'])
+              halt 400, "A profile with this email address already exists."
             else
-              newProfile[:full_name] = newProfile.delete :name
-              newProfile['active'] = true
-              newProfile.delete('password')
-              profInDB = DATABASE[:profiles].insert_one(newProfile)
-              url = 'http://localhost:8080/#/confirmation/' + profInDB.inserted_id.to_s
+              profile[:full_name] = profile.delete :name
+              profile['active'] = true
+              profile.delete('password')
+              profile = Profile.create(profile)
+              url = 'http://wernextgeneration.org/#/confirmation/' + profile.id
               sendEmail(
-                newProfile['email'],
-                'no-reply@wernextgeneration.org',
-                'WeRNextGeneration - Sign Up Confirmation',
-                "Navigate to this link to activate your account: #{url}",
-                "Follow the link below to activate your account: <br><br> <a href=\"#{url}\">Activate Account</a>"
+                to_addresses_array: [profile.email],
+                reply_addresses_array: ["no-reply@wernextgeneration.org"],
+                subject: "WeRNextGeneration - Sign Up Confirmation",
+                text: "Navigate to this link to activate your account: #{url}",
+                html: "Follow the link below to activate your account: <br><br> <a href=\"#{url}\">Activate Account</a>"
               )
-              200
+              json(profile)
             end
           end
 
           get_profile_by_id = lambda do
-            profile_id = params[:profile_id]
-            obj_id = BSON::ObjectId(profile_id)
-            profile_table = DATABASE[:profiles]
-            query_results = profile_table.find(:_id => obj_id)
-            match = query_results.first
-            json(match.to_h)
+            json(Profile.find(params[:profile_id]))
           end
 
           get_profile_by_session_token = lambda do
-            if (params[:_id]) != @token
+            if (params[:session_token]) != @token
               halt(401, "Invalid Token")
             else
-              checkedSession = DATABASE[:sessions].find(:_id => BSON::ObjectId(params[:_id])).first
-              user = DATABASE[:profiles].find(:email => checkedSession[:email]).first
-              json user
+              profile = Profile.find_by(email: Session.find(params[:session_token]).email)
+              json(profile)
             end
-          end
-
-          update_profile_role = lambda do
-            idnumber = params.delete("id")
-
-            if !@profile || @profile[:role] != 'superadmin'
-              if !checkParameters(params, profileParams)
-                halt 400, "the requirements were not met, did not post to database"
-              end
-            end
-
-            json DATABASE[:profiles].update_one(
-              {'_id' => BSON::ObjectId(idnumber)}, {'$set' => {role: params[:role]} }
-            )
           end
 
           update_profile = lambda do
-            content_type :json
-            formData = params['params']
-            DATABASE[:profiles].find(:_id => BSON::ObjectId(params[:id])).
-              update_one('$set' => {
-                'full_name' => formData['full_name'],
-                'email' => formData['email'],
-                'password' => formData['password'],
-              },)
-            updatedInfo = DATABASE[:profiles].find(:_id => BSON::ObjectId(params[:id])).first.to_h
-            json updatedInfo
+            if !@profile || @profile[:role] != 'superadmin'
+              if !checkParameters(params, profileParams)
+                halt 400, "Parameter requirements were not met."
+              end
+            end
+
+            profile = Profile.find(params[:id])
+            profile.update_attributes(params)
+            json(profile)
           end
 
           delete_profile = lambda do
-            DATABASE[:profiles].delete_one( {_id: BSON::ObjectId(params[:_id]) } )
+            profile = Profile.find(params[:id])
+            if profile
+              profile.destroy
+              json(profile)
+            else
+              halt 404, "No profile found with that ID."
+            end
           end
 
           activate_profile = lambda do
-            if params[:_id] && DATABASE[:profiles].find(:_id => BSON::ObjectId(params[:_id])).first
-              profile = DATABASE[:profiles].find(:_id => BSON::ObjectId(params[:_id])).first
-              if !profile['active']
-                json DATABASE[:profiles].update_one({:_id =>BSON::ObjectId(params[:_id])}, {'$set' => {active: true}})
-              else
-                halt 200, "profile has already been activated"
-              end
+            profile = Profile.find(params[:id])
+            if profile
+              profile.update_attributes(active: true)
+              json(profile)
             else
-              halt 400, "profile ID invalid, could not activate account"
+              halt 404, "No profile found with that ID."
             end
           end
 
           reset_profile_password = lambda do
-            email = params[:email]
-            profile = DATABASE[:profiles].find(:email => email).first
+            profile = Profile.find_by(email: params[:email])
+
             if !profile || !profile[:active]
-              halt 400, "there is no active profile with that email"
+              halt 404, "No profile found with that email."
             end
-            md5 = Digest::MD5.new
-            md5.update (email + DateTime.now().to_s)
-            DATABASE[:profiles].update_one({:email => email}, {'$set' => {resetToken: md5.hexdigest}})
-            url = 'http://localhost:8080/#/newPassword/' + md5.hexdigest
+
+            hex_digest = Digest::SHA256.hexdigest(profile.email + DateTime.now().to_s)
+            profile.update_attributes(reset_token: hex_digest)
+
+            url = 'http://wernextgeneration.org/#/updatePassword/' + hex_digest
             sendEmail(
-              email,
-              'no-reply@wernextgeneration.org',
-              'WeRNextGeneration - Password Reset',
-              'Click on the following link to reset your password: #{url}',
-              "Follow the link below to reset your password: <br><br> <a href=\"#{url}\">Reset Password</a>"
+              to_addresses_array: [profile.email],
+              reply_addresses_array: ["no-reply@wernextgeneration.org"],
+              subject: "WeRNextGeneration - Password Reset",
+              text: 'Click on the following link to reset your password: #{url}',
+              html: "Follow the link below to reset your password: <br><br> <a href=\"#{url}\">Reset Password</a>"
             )
-            200
+
+            json(profile)
           end
 
           update_password = lambda do
-            profile = DATABASE[:profiles].find(:resetToken => params[:resetToken]).first
+            profile = Profile.find_by(reset_token: params[:reset_token])
             if profile && profile[:active]
               password_hash = createPasswordHash(params[:password])
-              DATABASE[:profiles].update_one({:resetToken => params[:resetToken]}, {'$set' => {password_hash: password_hash, resetToken: ''}})
-              200
+              profile.update_attributes(password_hash: password_hash, reset_token: nil)
+              json(profile)
             else
-              halt 400, "no profile found with that reset token"
+              halt 404, "No profile found with that reset token."
             end
           end
  
+          app.get '/api/v1/profiles', &get_all_profiles
           app.post '/api/v1/profiles', &create_a_profile
           app.get '/api/v1/profiles/:profile_id', &get_profile_by_id
-          app.get '/api/v1/profile/:_id', &get_profile_by_session_token
-          app.put '/api/v1/profiles/:id', &update_profile_role
+          app.get '/api/v1/profile/:session_token', &get_profile_by_session_token
+          app.put '/api/v1/profiles/:id', &update_profile
           app.post '/api/v1/profile/edit/:id', &update_profile
-          app.delete '/api/v1/profiles/:_id', &delete_profile
-          app.put '/api/v1/profiles/activate/:_id', &activate_profile
-          app.put '/api/v1/profiles/resetPassword', &reset_profile_password
-          app.put '/api/v1/profiles/newPassword', &update_password
+          app.delete '/api/v1/profiles/:id', &delete_profile
+          app.put '/api/v1/profiles/activate/:id', &activate_profile
+          app.get '/api/v1/resetPassword/:email', &reset_profile_password
+          app.put '/api/v1/updatePassword/:reset_token', &update_password
 
         end
 
